148f8703a41717e7b0be261ed5a98d60807968e7
RocketPookie <nedychini@gmail.com>
2025-08-30 22:20:59 -0300

    Score

---
+++ 2DGameJam/Assets/Score.cs
@@ -0,0 +1,12 @@
+
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    public GameObject player;
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- 2DGameJam/Assets/Scripts/PlayerCollisions.cs
+++ 2DGameJam/Assets/Scripts/PlayerCollisions.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerCollisions : MonoBehaviour
 {
     public GameObject PanelUi;
 
     private int scorePoint = 0;
+    public Text scoreText;
     void muerte()
     {
         //todo: para jego , guardar, abrir ui
@@ -25,6 +27,7 @@ public class PlayerCollisions : MonoBehaviour
             scorePoint += 1;
             Debug.Log(scorePoint);
             collision.SendMessage("kaboom");
+            scoreText.text = scorePoint.ToString();
         }
     }
 }
