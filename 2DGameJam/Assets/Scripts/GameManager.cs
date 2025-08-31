c359d92ccb6735af1e5025ffc72ab99e22b58f9e
RocketPookie <nedychini@gmail.com>
2025-08-30 22:01:11 -0300

    Menues

--- 2DGameJam/Assets/Scripts/GameManager.cs
+++ 2DGameJam/Assets/Scripts/GameManager.cs
@@ -8,5 +8,17 @@ public class GameManager : MonoBehaviour
     public void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        Time.timeScale = 1;
+    }
+
+    public void playGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void returnMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
