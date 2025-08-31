c3efa5eebbfa3507fb2630b857a7bb13818b5af3
RocketPookie <nedychini@gmail.com>
2025-08-31 10:04:15 -0300

    p

--- 2DGameJam/Assets/Scripts/Parallax.cs
+++ 2DGameJam/Assets/Scripts/Parallax.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Parallax : MonoBehaviour
 {
 
-    public Transform[] fondos; 
+    public Transform[] fondos;     //almacena los fondos a ser afectados por el parallax
+    private float[] parallaxScales; //la cantidad de movimiento que va a tener la camara para cada elemento
     void Start()
     {
         
